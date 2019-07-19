6e9d21d5e93e7e935f1310bd8c684af774b7860a
resurrected21 <prabhjot.sookshminfo@gmail.com>
2019-07-19 09:05:34 +0000

    update build.sh

--- build.sh
+++ build.sh
@@ -3,13 +3,15 @@
 #
 # Script to compile NightowlKernel Image for jalebi
 #
-export ARCH=arm64
-export SUBARCH=arm64
-#export CROSS_COMPILE=~/gcc4/bin/aarch64-linux-android-
-export CROSS_COMPILE=~/gcc64/bin/aarch64-linux-gnu-
 
-rm -rf out
-mkdir -p out
-make O=out on5xelte_00_defconfig
-make O=out
+export ANDROID_MAJOR_VERSION=o
+export ARCH=arm64
+#export SUBARCH=arm64
+export CROSS_COMPILE=~/tc/bin/aarch64-linux-android-
+#export CROSS_COMPILE=~/gcc64/bin/aarch64-linux-gnu-
+
+#rm -rf out
+#mkdir -p out
+make on5xelte_O_defconfig
+make
 
